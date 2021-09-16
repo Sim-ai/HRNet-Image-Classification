3d3d8d9e51c2a50bf3ce7d1d63c01e162ba66b0e
Sim-ai <71258346+Sim-ai@users.noreply.github.com>
2021-09-16 21:41:24 +0800

    Update train.py

--- tools/train.py
+++ tools/train.py
@@ -22,6 +22,7 @@ import torch.optim
 import torch.utils.data
 import torch.utils.data.distributed
 import torchvision.datasets as datasets
+from torch.utils.data import Dataset   # add by xp
 import torchvision.transforms as transforms
 from tensorboardX import SummaryWriter
 
@@ -35,6 +36,44 @@ from utils.modelsummary import get_model_summary
 from utils.utils import get_optimizer
 from utils.utils import save_checkpoint
 from utils.utils import create_logger
+from utils.utils import read_split_data
+
+class Hrnet_16bit(Dataset):
+    """自定义数据集"""
+    def __init__(self, images_path: list, images_class: list, transform=None):
+        self.images_path = images_path       # 训练集所有图片的路径列表。
+        self.images_class = images_class     # 训练样本的标签信息。
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images_path)     # 返回训练集下所有的数据个数。
+
+    def __getitem__(self, item):         # 传入的item为索引。
+        img4 = Image.open(self.images_path[item])   # self.images_path获得索引对应图片的路径。
+        # RGB为彩色图片，L为灰度图片
+        if img4.mode != 'I;16':
+            raise ValueError("image: {} isn't I;16 mode.".format(self.images_path[item]))
+        img3 = img4.convert("I")
+        trans1=transforms.Compose([transforms.RandomResizedCrop(224),
+                                 transforms.RandomHorizontalFlip(),
+                                 transforms.ToTensor()])
+        img2 = trans1(img3)
+        img = torch.div(img2.float(), 65535.0)
+        label = self.images_class[item]   # 将item传入得到数字索引。
+
+        return img, label   # 返回图片以及标签。
+
+    @staticmethod   # @staticmethod为装饰器，表明该方法为静态方法。
+    def collate_fn(batch):
+        # 官方实现的default_collate可以参考
+        # https://github.com/pytorch/pytorch/blob/67b7e751e6b5931a9f45274653f4f653a4e6cdf6/torch/utils/data/_utils/collate.py
+        images, labels = tuple(zip(*batch))  #通过非关键字参数的形式传入zip方法，这样图片和图片在一起，标签和标签在一起。
+                                             #每个image对应的维度是[3,224,224]
+
+        images = torch.stack(images, dim=0)  # 将images通过torch.stack方法进行拼接，stack方法会增加新的维度，即bath的数量，指定新的维度在dim=0的位置上。
+                                             # stack后的数据维度是[8,3,224,224]
+        labels = torch.as_tensor(labels)     # 将labels也转化成tensor的形式。
+        return images, labels
 
 
 def parse_args():
@@ -84,9 +123,13 @@ def main():
     model = eval('models.'+config.MODEL.NAME+'.get_cls_net')(
         config)
 
+#     dump_input = torch.rand(
+#         (1, 3, config.MODEL.IMAGE_SIZE[1], config.MODEL.IMAGE_SIZE[0])
+#     )                                                                       # commented by xp and changed as below
     dump_input = torch.rand(
-        (1, 3, config.MODEL.IMAGE_SIZE[1], config.MODEL.IMAGE_SIZE[0])
+        (1, 1, config.MODEL.IMAGE_SIZE[1], config.MODEL.IMAGE_SIZE[0])
     )
+    
     logger.info(get_model_summary(model, dump_input))
 
     # copy model file
@@ -138,21 +181,34 @@ def main():
         )
 
     # Data loading code
-    traindir = os.path.join(config.DATASET.ROOT, config.DATASET.TRAIN_SET)
-    valdir = os.path.join(config.DATASET.ROOT, config.DATASET.TEST_SET)
+#     traindir = os.path.join(config.DATASET.ROOT, config.DATASET.TRAIN_SET)
+#     valdir = os.path.join(config.DATASET.ROOT, config.DATASET.TEST_SET)
+#     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+#                                      std=[0.229, 0.224, 0.225])
+    normalize = transforms.Normalize(mean=[0.5],
+                                     std=[0.5])
 
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
 
-    train_dataset = datasets.ImageFolder(
-        traindir,
-        transforms.Compose([
-            transforms.RandomResizedCrop(config.MODEL.IMAGE_SIZE[0]),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ])
-    )
+    traindir = '/xiaopeng/data10/train/'
+    valdir = '/xiaopeng/data10/val/'
+
+    
+#     train_dataset = datasets.ImageFolder(
+#         traindir,
+#         transforms.Compose([
+#             transforms.RandomResizedCrop(config.MODEL.IMAGE_SIZE[0]),
+#             transforms.RandomHorizontalFlip(),
+#             transforms.ToTensor(),
+#             normalize,
+#         ])
+#     )
+    train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(traindir)
+    train_dataset = Hrnet_16bit(images_path=train_images_path,
+                              images_class=train_images_label,
+                              transform=None)
+    valid_dataset = Hrnet_16bit(images_path=val_images_path,
+                              images_class=val_images_label,
+                              transform=None)
     #以下为增加的代码，上面几行是原有的代码
     #print(train_dataset.classes)  #根据分的文件夹的名字来确定的类别
     with open("class.txt","w") as f1:
@@ -175,15 +231,23 @@ def main():
         pin_memory=True
     )
 
+#     valid_loader = torch.utils.data.DataLoader(
+#         datasets.ImageFolder(valdir, transforms.Compose([
+#             transforms.Resize(int(config.MODEL.IMAGE_SIZE[0] / 0.875)),
+#             transforms.CenterCrop(config.MODEL.IMAGE_SIZE[0]),
+#             transforms.ToTensor(),
+#             normalize,
+#         ])),
+#         batch_size=config.TEST.BATCH_SIZE_PER_GPU*len(gpus),
+#         shuffle=False,
+#         num_workers=config.WORKERS,
+#         pin_memory=True
+#     )                                                                 # commented by xp and changed as below
+
     valid_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(valdir, transforms.Compose([
-            transforms.Resize(int(config.MODEL.IMAGE_SIZE[0] / 0.875)),
-            transforms.CenterCrop(config.MODEL.IMAGE_SIZE[0]),
-            transforms.ToTensor(),
-            normalize,
-        ])),
-        batch_size=config.TEST.BATCH_SIZE_PER_GPU*len(gpus),
-        shuffle=False,
+        valid_dataset,
+        batch_size=config.TRAIN.BATCH_SIZE_PER_GPU*len(gpus),
+        shuffle=True,
         num_workers=config.WORKERS,
         pin_memory=True
     )
